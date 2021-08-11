7b98361c6a511601c29af5daa7677699ea99fe7b
yashwant-c <74008172+yashwant-c@users.noreply.github.com>
2021-08-11 18:41:23 +0530

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,10 @@
+function Back()
+{
+    window.location = "activity_1.html";
+}
+
+function GetScore()
+{
+    score=localStorage.getItem("score");
+    document.getElementById("update").innerHTML="<h1>Score:"+score"</h1>";
+}
